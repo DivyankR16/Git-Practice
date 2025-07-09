be5d5d4675831645cae5b3c169932aa9181da333
Divyank0101 <enigmawizard030@gmail.com>
2025-07-10 00:33:32 +0530

    feat: Written linear searching function and also prepared gitignore

---
+++ DSA/Searching/linear_search.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define forn(i,n) for(ll i=0;i<n;i++)
+
+void solve(){
+    ll n,target;
+    cin >> n>>target;
+    vector<ll> a(n);
+    forn(i, n) cin >> a[i];
+    bool res = false;
+    for (ll i = 0; i < a.size();i++){
+        if(a[i]==target){
+            res = true;
+            break;
+        }
+    }
+    if(res){
+        cout << "FOUND" << "\n";
+    }
+    else{
+        cout << "UNKNOWN" << "\n";
+    }
+}
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    ll t;
+    cin>>t;
+    while(t--){
+    solve();
+    }
+    return 0;
+}
